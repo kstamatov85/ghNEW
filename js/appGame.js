19c3be52d691eba8256f5e82d7275b537f9bd914
K.Stamatov <kaloyan.stamatov@internovusbg.com>
2014-12-08 14:49:49 +0200

    add pointer and fix wind arrow

--- js/appGame.js
+++ js/appGame.js
@@ -64,12 +64,14 @@
                 // load walls
                 this.load.image('gameBG-1', 'img/pattern6.jpg');
                 
-                //Load mouth
+                //Load hole
                 this.load.image('hole-1', 'img/hole.png');
                 
                 //Load mouth
                 this.load.image('mouth-1', 'img/mouth1.jpg');
                 
+                //Load balls
+                this.load.image('ball-1', 'img/ball.png');
                 
                 // load spritesheets
                 this.load.spritesheet('button-start', 'img/button-start.png', 401, 143); 
@@ -123,6 +125,8 @@
             $rootScope.gh.p_scoreText = null;
             $rootScope.gh.p_timeAvab = null;
             $rootScope.gh.pointer = null;
+
+            
             //PRIVATE PARAMS
             this.g_fontStyle = {font: "60px Arial", fill: "#FF0000", stroke: "#333333", strokeThickness: 5, align: "center"};
         
@@ -139,28 +143,33 @@
                 
                 // Background
                 var bg = this.add.tileSprite(0, 0, $rootScope.gh.GAME_WIDTH, $rootScope.gh.GAME_HEIGHT, 'gameBG-1');
+                
                 // Score 
                 $rootScope.gh.p_scoreText = this.add.text(50, 50, "0", this.g_fontStyle);
+                
                 // Timer
                 $rootScope.gh.p_timeAvab = this.add.text($rootScope.gh.GAME_WIDTH/2, 50, $rootScope.gh.p_timer, this.g_fontStyle);
                 $rootScope.gh.p_timeAvab.anchor.setTo(0.5, 0.5);
+                this.time.events.loop(Phaser.Timer.SECOND, this.countDown, this);
+                
                 // Add Pause button
                 this.add.button($rootScope.gh.GAME_WIDTH-100, 50, 'pauseIcon', this.managePause, this);
+                
                 // Wind Arrow
                 $rootScope.gh.windAngle = this.add.sprite($rootScope.gh.GAME_WIDTH/2, 200, 'windArrow', this);
                 $rootScope.gh.windAngle.anchor.setTo(0.5, 0.5);
                 $rootScope.gh.windAngle.scale.setTo(0.5, 0.5);
                 $rootScope.gh.windAngle.angle = 90;
-               
+
+                
+
                 //HOLE AND MOUTH
                 var mouth = this.add.sprite($rootScope.gh.GAME_WIDTH/2, 500, 'mouth-1');
                 var hole = this.add.sprite( $rootScope.gh.GAME_WIDTH/2, 500, 'hole-1');
                 mouth.anchor.setTo(0.5, 0.5);
                 hole.anchor.setTo(0.5, 0.5);
-                
                 mouth.width = hole.width - 70;
                 mouth.height = hole.height - 70;
-                
                 //ADD MASK TO THE MOUTH
                 var graphics = game.add.graphics($rootScope.gh.GAME_WIDTH/2, 500);
                 graphics.beginFill();
@@ -172,11 +181,21 @@
                 $rootScope.gh.pointer = this.add.sprite($rootScope.gh.GAME_WIDTH/2, $rootScope.gh.GAME_HEIGHT, 'windArrow', this);
                 $rootScope.gh.pointer.anchor.setTo(0.5, 0.5);
                 $rootScope.gh.pointer.scale.setTo(0.5, 0.5);
+                $rootScope.gh.pointer.angle = 50;
+                game.add.tween($rootScope.gh.pointer).to({angle:-50}, 1200, Phaser.Easing.Linear.None, true, 0, 1200, true);
                 
-                //RUN COUNTER
-                this.time.events.loop(Phaser.Timer.SECOND, this.countDown, this);
+                
+                
+                this.inputEnabled = true;
+                this.input.onDown.add(this.clickScore, this);
+                //this.events.onInputDown.add(this.clickScore, this);
  
             },
+            update: function() {
+                //$rootScope.gh.windAngle.angle = game.rnd.angle();
+                
+            },
+            // MANAGE PAUSE FUNCTION
             managePause: function() {
                 this.game.paused = true;
                 var pausedText = this.add.text(100, 450, "Game paused.\nTap anywhere to continue.", this.g_fontStyle);
@@ -185,26 +204,25 @@
                     this.game.paused = false;
                 }, this);
             },
-            update: function() {
-                //$rootScope.gh.windAngle.angle = game.rnd.angle();
-                
-            },
-            clickScore: function(){ //CURRENTLY NOT USED
-                console.log('sss');
-                $rootScope.gh.p_score+=1;
-                    $rootScope.gh.p_scoreText.setText($rootScope.gh.p_score);
-            },
+            // TIME COUNTER
             countDown : function(){
-                if($rootScope.gh.p_timer % 2 === 0 ){
+            
+                //Change wind on every 5 secs
+                if($rootScope.gh.p_timer % 5 === 0 ){
                     var angleVal = Math.cos( Math.PI * Math.round( Math.random() ) );
-                    //game.add.tween(mummy).to({ x: game.width + (1600 + mummy.x) }, 20000, Phaser.Easing.Linear.None, true);
                     game.add.tween($rootScope.gh.windAngle).to({angle:90*angleVal}, 500, Phaser.Easing.Linear.None, true);
-                    //$rootScope.gh.windAngle.angle = 90*angleVal;
                 }
+                
+                //Handle with timer
                 $rootScope.gh.p_timer-=1;
                 if($rootScope.gh.p_timer === 0 ){this.time.events.stop()}
                 $rootScope.gh.p_timeAvab.setText($rootScope.gh.p_timer);
+            },
+            clickScore: function(){ //CURRENTLY NOT USED
+                console.log('sss');
+               
             }
+           
             
         };
         
